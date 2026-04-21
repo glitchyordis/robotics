835a8a34a15ab90a469881be659219a62f13fed7
Felix Jia Xun Kong <felix.kong@keysight.com>
2026-04-21 12:49:02 +0800

    Update hande attachement

--- test_build_from_urdf.py
+++ test_build_from_urdf.py
@@ -1,32 +1,17 @@
-import math
 import os
 
-import numpy as np
-import pink
 import pinocchio as pin
-from pink import solve_ik
-from pink.tasks import FrameTask, PostureTask
 from pink.visualization import start_meshcat_visualizer
 from robot_descriptions.loaders.pinocchio import load_robot_description
 
-# # 1. Load the UR5e Arm from robot_descriptions
-# arm = load_robot_description("ur5e_description")
-
-# # 2. Load the Hand-E Gripper (Local URDF)
-# # Make sure this path points to where your generated hande.urdf is
-# urdf_path = "hande.urdf" 
-# # This path should be the FOLDER that contains 'robotiq_hande_description'
-# # so Pinocchio can find the meshes.
-# mesh_dir = os.getcwd() 
-
-# gripper = pin.RobotWrapper.BuildFromURDF(
-#     urdf_path,
-#     package_dirs=[mesh_dir]
-# )
-
-
 arm = load_robot_description("ur5e_description")
-gripper = pin.RobotWrapper.BuildFromURDF("hande.urdf", package_dirs=[...])
+gripper_urdf = os.path.join(os.getcwd(), "hande.urdf")
+package_dir = os.path.join(os.getcwd(), "src")
+
+gripper = pin.RobotWrapper.BuildFromURDF(
+    gripper_urdf,
+    package_dirs=[package_dir],
+)
 
 tool0_id = arm.model.getFrameId("tool0")
 placement = pin.SE3.Identity()
@@ -44,3 +29,9 @@ _, visual_model = pin.appendModel(
 )
 
 robot = pin.RobotWrapper(model, collision_model, visual_model)
+
+viz = start_meshcat_visualizer(robot)
+viz.display(pin.neutral(robot.model))
+
+input("Meshcat is running. Press Enter to exit.\n")
+
